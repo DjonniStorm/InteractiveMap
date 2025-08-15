9bacdbf401af2407e09225bcca211101c1602c64
DjonniStorm <i.pazushkin@yandex.ru>
2025-08-15 22:29:12 +0400

    fix: lint-errors 2

--- src/app/providers/mantine.tsx
+++ src/app/providers/mantine.tsx
@@ -3,14 +3,9 @@ import type { PropsWithChildren } from 'react';
 import '@mantine/core/styles.css';
 
 import { MantineProvider } from '@mantine/core';
-import { ModalsProvider } from '@mantine/modals';
 
 const Mantine = ({ children }: PropsWithChildren) => {
-  return (
-    <MantineProvider>
-      <ModalsProvider>{children}</ModalsProvider>
-    </MantineProvider>
-  );
+  return <MantineProvider>{children}</MantineProvider>;
 };
 
 export { Mantine };
